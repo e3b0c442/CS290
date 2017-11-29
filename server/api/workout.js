1eba7da801e86b6093bcc344d9fcdb62034324b2
Nick Meyer <nimeyer@yahoo-inc.com>
2017-11-29 00:02:01 -0600

    moment utc

--- server/api/workout.js
+++ server/api/workout.js
@@ -1,104 +1,118 @@
 const express = require('express');
-const moment = require('moment');
 const workout = require('../db/workout');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    workout.list().then((rows) => {
-        res.status(200).send(rows);
-    }).catch((err) => {
-        res.status(500).send(err);
+  workout
+    .list()
+    .then(rows => {
+      res.status(200).send(rows);
     })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
 
 router.post('/', (req, res) => {
-    let ok = true;
-    const data = {};
-    ['name','reps','weight','date','lbs'].forEach((field) => {
-        try {
-            const validated = workout.validate(field, req.body[field]);
-            data[field] = validated;
-        } catch (err) {
-            console.log(err);
-            res.status(400).send({
-                error: "Validation failed",
-                detail: err
-            });
-            ok = false
-        }
-    });
-
-    if(ok) {
-        workout.insert(data).then((rows) => {
-            res.status(200).send(rows);
-        }).catch((err) => {
-            res.status(500).send(err);
-        });
+  let ok = true;
+  const data = {};
+  ['name', 'reps', 'weight', 'date', 'lbs'].forEach(field => {
+    try {
+      const validated = workout.validate(field, req.body[field]);
+      data[field] = validated;
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({
+        error: 'Validation failed',
+        detail: err
+      });
+      ok = false;
     }
+  });
+
+  if (ok) {
+    workout
+      .insert(data)
+      .then(rows => {
+        res.status(200).send(rows);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  }
 });
 
 router.post('/:workoutId', (req, res) => {
-    let id;
-    try {
-        id = workout.validate("id", req.params.workoutId)
-    } catch(err) {
-        res.status(400).send({
-            error: "Invalid ID",
-            detail: `ID ${req.params.workoutId} does not exist or is invalid`
-        });
-        return;
-    }
-
-    let ok = true;
-    const data = {};
-    ['name','reps','weight','date','lbs'].forEach((field) => {
-        try {
-            const validated = workout.validate(field, req.body[field]);
-            data[field] = validated;
-        } catch (err) {
-            console.log(err);
-            res.status(400).send({
-                error: "Validation failed",
-                detail: err
-            });
-            ok = false
-        }
+  let id;
+  try {
+    id = workout.validate('id', req.params.workoutId);
+  } catch (err) {
+    res.status(400).send({
+      error: 'Invalid ID',
+      detail: `ID ${req.params.workoutId} does not exist or is invalid`
     });
+    return;
+  }
 
-    if(ok) {
-        workout.update(id, data).then((rows) => {
-            res.status(200).send(rows);
-        }).catch((err) => {
-            res.status(500).send(err);
-        });
-    }
-})
-
-router.delete("/:workoutId", (req, res) => {
-    let id;
+  let ok = true;
+  const data = {};
+  ['name', 'reps', 'weight', 'date', 'lbs'].forEach(field => {
     try {
-        id = workout.validate("id", req.params.workoutId)
-    } catch(err) {
-        res.status(400).send({
-            error: "Invalid ID",
-            detail: `ID ${req.params.workoutId} does not exist or is invalid`
-        });
-        return;
+      const validated = workout.validate(field, req.body[field]);
+      data[field] = validated;
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({
+        error: 'Validation failed',
+        detail: err
+      });
+      ok = false;
     }
+  });
 
-    workout.delete(id).then((rows) => {
+  if (ok) {
+    workout
+      .update(id, data)
+      .then(rows => {
         res.status(200).send(rows);
-    }).catch((err) => {
+      })
+      .catch(err => {
         res.status(500).send(err);
+      });
+  }
+});
+
+router.delete('/:workoutId', (req, res) => {
+  let id;
+  try {
+    id = workout.validate('id', req.params.workoutId);
+  } catch (err) {
+    res.status(400).send({
+      error: 'Invalid ID',
+      detail: `ID ${req.params.workoutId} does not exist or is invalid`
     });
-})
+    return;
+  }
+
+  workout
+    .delete(id)
+    .then(rows => {
+      res.status(200).send(rows);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 function reset(req, res) {
-    workout.reset().then((rows) => {
-        res.status(200).send(rows);
-    }).catch((err) => {
-        res.status(500).send(err);
+  workout
+    .reset()
+    .then(rows => {
+      res.status(200).send(rows);
+    })
+    .catch(err => {
+      res.status(500).send(err);
     });
 }
 
--- server/db/workout.js
+++ server/db/workout.js
@@ -2,115 +2,111 @@ const moment = require('moment');
 const db = require('./db');
 
 const fields = {
-    id: "int",
-    name: "string",
-    reps: "int",
-    weight: "int",
-    date: "date",
-    lbs: "bool",
+  id: 'int',
+  name: 'string',
+  reps: 'int',
+  weight: 'int',
+  date: 'date',
+  lbs: 'bool'
 };
 
 function validate(field, value) {
+  if (value === undefined) {
+    throw `Field ${field} is missing a value`;
+  }
 
-    if (value === undefined) {
-        throw `Field ${field} is missing a value`;
-    }
-
-    let val;
-    switch(fields[field]) {
-        case "string":
-            if(value.length < 1) {
-                throw `Field "${field}" is empty; value required`;
-            }
-            return value;
-        case "int":
-            val = parseInt(value);
-            if(val === NaN) {
-                throw `Field "${field}" is not a valid integer`;
-            }
-            return val;
-        case "date":
-            val = moment(value);
-            if(!val.isValid()) {
-                throw `Field "${field}" is not a valid ISO8601-formatted date`;
-            }
-            return val.toDate();
-        case "bool":
-            val = Boolean(value);
-            return val;
-        default: 
-            throw `Field "${field}" does not have a validation type set`;
-    }
+  let val;
+  switch (fields[field]) {
+    case 'string':
+      if (value.length < 1) {
+        throw `Field "${field}" is empty; value required`;
+      }
+      return value;
+    case 'int':
+      val = parseInt(value);
+      if (val === NaN) {
+        throw `Field "${field}" is not a valid integer`;
+      }
+      return val;
+    case 'date':
+      val = moment.utc(value);
+      if (!val.isValid()) {
+        throw `Field "${field}" is not a valid ISO8601-formatted date`;
+      }
+      return val.toDate();
+    case 'bool':
+      val = Boolean(value);
+      return val;
+    default:
+      throw `Field "${field}" does not have a validation type set`;
+  }
 }
 
 function list() {
-    return new Promise((resolve, reject) => {
-        db.query("SELECT * FROM workouts ORDER BY id DESC;", (err, rows) => {
-            if(err) {
-                reject(err);
-                return;
-            }
-            resolve(rows);
-        });
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM workouts ORDER BY id DESC;', (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(rows);
     });
+  });
 }
 
 function insert(data) {
-    return new Promise((resolve, reject) => {
-        db.query(
-            "INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?);",
-            [data.name, data.reps, data.weight, data.date, data.lbs],
-            (err, rows) => {
-                if(err) {
-                    reject(err);
-                    return;
-                }
-                resolve(rows);
-            }
-        );
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      'INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?);',
+      [data.name, data.reps, data.weight, data.date, data.lbs],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      }
+    );
+  });
 }
 
 function update(id, data) {
-    return new Promise((resolve, reject) => {
-        db.query(
-            "UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?",
-            [data.name, data.reps, data.weight, data.date, data.lbs, id],
-            (err, rows) => {
-                if(err) {
-                    reject(err);
-                    return;
-                }
-                resolve(rows);
-            }  
-        );
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      'UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?',
+      [data.name, data.reps, data.weight, data.date, data.lbs, id],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      }
+    );
+  });
 }
 
 function del(id) {
-    return new Promise((resolve, reject) => {
-        db.query(
-            "DELETE FROM workouts WHERE id=?",
-            [id],
-            (err, rows) => {
-                if(err) {
-                    reject(err);
-                    return;
-                }
-                resolve(rows);
-            }
-        );
+  return new Promise((resolve, reject) => {
+    db.query('DELETE FROM workouts WHERE id=?', [id], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(rows);
     });
+  });
 }
 
 function reset() {
-    return new Promise((resolve, reject) => {
-        db.query("DROP TABLE IF EXISTS workouts;", (err, result) => {
-            if(err) {
-                reject(err);
-                return;
-            }
-            db.query(`
+  return new Promise((resolve, reject) => {
+    db.query('DROP TABLE IF EXISTS workouts;', (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      db.query(
+        `
                 CREATE TABLE workouts(
                     id INT PRIMARY KEY AUTO_INCREMENT,
                     name VARCHAR(255) NOT NULL,
@@ -119,15 +115,17 @@ function reset() {
                     date DATE,
                     lbs BOOLEAN
                 );
-            `, (err) => {
-                if(err) {
-                    reject(err);
-                    return;
-                }
-                resolve(result);
-            });
-        });
+            `,
+        err => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(result);
+        }
+      );
     });
+  });
 }
 
 module.exports.list = list;
